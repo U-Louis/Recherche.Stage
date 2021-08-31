3353e9e5878163af7c339dbc75f1362291cbe09d
Louis <69362039+U-Louis@users.noreply.github.com>
2021-08-31 15:45:18 +0200

    +nav wip

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,14 @@
-/* let eng = document.getElementById("eng");
-eng.innerHTML = ""; */
+let eng = document.getElementById("eng");
+let fr = document.getElementById("fr");
+let engBtn = document.getElementById("btnradio1");
+engBtn.onclick = function() { setLangage(eng, fr); }
+let frBtn = document.getElementById("btnradio2");
+frBtn.onclick = function() { setLangage(fr, eng); }
+
+let setLangage = (langageIn, langageOut) => {
+        langageOut.style.display = "none";
+        langageIn.style.display = "block";
+    }
+    (function() {
+        setLangage(fr, eng);
+    })();
