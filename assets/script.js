bd9b782718d16f71ae0a0f45f9175ead1b57e4ab
Louis <louis.urbani@yahoo.com>
2021-08-31 21:04:19 +0200

    navbar OK

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
 let eng = document.getElementById("eng");
 let fr = document.getElementById("fr");
 let engBtn = document.getElementById("btnradio1");
-engBtn.onclick = function() { setLangage(eng, fr); }
+engBtn.onclick = function() { setLangage(eng, fr, engBtn, frBtn); }
 let frBtn = document.getElementById("btnradio2");
-frBtn.onclick = function() { setLangage(fr, eng); }
+frBtn.onclick = function() { setLangage(fr, eng, frBtn, engBtn); }
 
-let setLangage = (langageIn, langageOut) => {
-        langageOut.style.display = "none";
-        langageIn.style.display = "block";
-    }
-    (function() {
-        setLangage(fr, eng);
-    })();
+let setLangage = (langageIn, langageOut, btnIn, btnOut) => {
+    langageOut.style.display = "none";
+    langageIn.style.display = "block";
+    console.log(btnIn);
+    btnIn.style.filter = "grayscale(0%)";
+    btnOut.style.filter = "grayscale(100%)";
+}
+
+(function() {
+    setLangage(fr, eng, frBtn, engBtn);
+})();
